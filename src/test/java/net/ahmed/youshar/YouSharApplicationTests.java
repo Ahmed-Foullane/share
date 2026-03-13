2916ab887d300e62d6db4214be4c58f83633a80b
Ahmed-Foullane <ahmedfoullane@gmail.com>
2026-03-13 11:47:28 +0000

    feat: intial spring boot project
    -dockerize project: add Dockerfile and docker-compose configuration
    -configer application properties
    -add pom dependencyes

---
+++ src/test/java/net/ahmed/youshar/YouSharApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ahmed.youshar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouSharApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
